e9452a327098df3165071b7978f1dfcd6d5ee239
01 <01@01-PC>
2016-12-17 16:43:00 +0800

    添加方法：生成成功后，打开生成的目录，避免手工打开

--- src/main/java/com/isoft/generator/App.java
+++ src/main/java/com/isoft/generator/App.java
@@ -1,5 +1,7 @@
 package com.isoft.generator;
 
+import java.io.File;
+
 import cn.org.rapid_framework.generator.GeneratorFacade;
 
 public class App {
@@ -9,10 +11,32 @@ public class App {
         g.getGenerator().addTemplateRootDir(templatePath);
         // g.getGenerator().setOutRootDir("C:\\download\\eclipse\\workspace\\generator\\generator-output");
         g.deleteOutRootDir();
-        g.generateByTable("MM_Material", "MM_Supplier"); // 可多表生成MM_InStock
-// C:\download\eclipse\workspace\generator\generator-output\java_src\main\java\com\isoftoon\ld\fx\model
-// C:\download\eclipse\workspace\generator\generator-output\java_src\main\java\com\isoftoon\ld\fx\service
-// C:\download\eclipse\workspace\generator\generator-output\java_src\main\java\com\isoftoon\ld\fx\controller
-// C:\download\eclipse\workspace\generator\generator-output\java_src\main\java\com\isoftoon\ld\fx\dialog
+        //g.generateByTable("MM_Stock", "MM_InStock", "MM_OutStock", "MM_Material", "MM_Supplier"); // 可多表生成MM_InStock
+        g.generateByTable("PP_TaskList"); // 可多表生成MM_InStock
+        openDirectory("C:\\download\\eclipse\\workspace\\generator\\generator-output\\java_src\\main\\java\\com\\isoftoon\\ld\\fx");
+    }
+
+    // 打开生成的目录[model，service，controller，dialog]
+    public static void openDirectory(String folder) {
+        File file = new File(folder);
+        if (!file.exists()) {
+            return;
+        }
+        Runtime runtime = null;
+        try {
+            runtime = Runtime.getRuntime();
+            //if (!SystemUtil.isWindows) {
+                // System.out.println("is linux");
+            //    runtime.exec("nautilus " + folder);
+            //} else {
+                runtime.exec("cmd /c start explorer " + folder);
+            //}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (null != runtime) {
+                runtime.runFinalization();
+            }
+        }
     }
 }
